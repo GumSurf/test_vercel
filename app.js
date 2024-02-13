07a90d8968cf93c8fb10cca02c7304305f42268d
GumSurf <gabriel.christe@gmail.com>
2024-02-13 19:55:55 +0100

    Add: app.js

--- app.js
+++ app.js
@@ -7,13 +7,6 @@ const swaggerUi = require('swagger-ui-express')
 const yaml = require('yamljs')
 const swaggerDocs = yaml.load('swagger.yaml')
 const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(helmet({
-      crossOriginResourcePolicy: false,
-    }));
-app.use('/images', express.static(path.join(__dirname, 'images')))
 
 const db = require("./models");
 module.exports = app;
