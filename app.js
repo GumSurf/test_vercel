8839a5a9305f597e739e06904e63da5ec57b156d
GumSurf <gabriel.christe@gmail.com>
2024-02-13 19:58:06 +0100

    Add: app.js

--- app.js
+++ app.js
@@ -2,9 +2,5 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors')
 require('dotenv').config();
-const helmet = require('helmet');
-const swaggerUi = require('swagger-ui-express')
-const yaml = require('yamljs')
-const swaggerDocs = yaml.load('swagger.yaml')
 const app = express()
 module.exports = app;
