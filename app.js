54fdda94f5030b867fb2cf4bf4eabeab51aa8302
GumSurf <gabriel.christe@gmail.com>
2024-02-13 19:54:47 +0100

    Add: app.js

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors')
 require('dotenv').config();
+const helmet = require('helmet');
+const swaggerUi = require('swagger-ui-express')
+const yaml = require('yamljs')
+const swaggerDocs = yaml.load('swagger.yaml')
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(helmet({
+      crossOriginResourcePolicy: false,
+    }));
+app.use('/images', express.static(path.join(__dirname, 'images')))
+
+const db = require("./models");
 module.exports = app;
