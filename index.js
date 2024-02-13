806b40859255ae09d84af6010f0b9aac59e465f2
GumSurf <gabriel.christe@gmail.com>
2024-02-13 19:13:12 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur mon serveur Express !');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Le serveur est en cours d'exécution sur le port ${PORT}`);
+});
