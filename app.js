6f5514c3fd45dbb007da005ea32cfdc0907c4b18
GumSurf <gabriel.christe@gmail.com>
2024-02-13 19:52:50 +0100

    Add: app.js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const express = require('express');
+require('dotenv').config();
 const app = express()
+
+const db = require("./models");
+const worksRoutes = require('./routes/works.routes');
+db.sequelize.sync().then(()=> console.log('db is ready'));
+app.use('/api/works', worksRoutes);
 module.exports = app;
