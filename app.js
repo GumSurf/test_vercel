5738da695127ef81519314b80f4a8872dd8625ab
GumSurf <gabriel.christe@gmail.com>
2024-02-13 19:49:32 +0100

    Add: app.js

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors')
 require('dotenv').config();
-const helmet = require('helmet');
 const swaggerUi = require('swagger-ui-express')
-const yaml = require('yamljs')
-const swaggerDocs = yaml.load('swagger.yaml')
 const app = express()
 
 app.use('/images', express.static(path.join(__dirname, 'images')))
