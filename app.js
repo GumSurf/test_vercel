c1b97b663c7857723efcad8d296d2ba392f99837
GumSurf <gabriel.christe@gmail.com>
2024-02-13 19:51:14 +0100

    Add: app.js

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
 const express = require('express');
-const path = require('path');
-require('dotenv').config();
-const swaggerUi = require('swagger-ui-express')
 const app = express()
-
-app.use('/images', express.static(path.join(__dirname, 'images')))
-
-const db = require("./models");
-const userRoutes = require('./routes/user.routes');
-const categoriesRoutes = require('./routes/categories.routes');
-const worksRoutes = require('./routes/works.routes');
-db.sequelize.sync().then(()=> console.log('db is ready'));
-app.use('/api/users', userRoutes);
-app.use('/api/categories', categoriesRoutes);
-app.use('/api/works', worksRoutes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 module.exports = app;
